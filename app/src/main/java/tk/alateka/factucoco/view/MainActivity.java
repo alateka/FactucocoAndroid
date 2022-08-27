98b70b6bb1137350d03abae63908075520a2cf5d
Alberto Pérez Fructuoso <albertoperez_135@outlook.es>
2022-08-27 13:16:28 +0200

    Initial commit

---
+++ app/src/main/java/tk/alateka/factucoco/view/MainActivity.java
@@ -0,0 +1,14 @@
+package tk.alateka.factucoco.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import tk.alateka.factucoco.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
