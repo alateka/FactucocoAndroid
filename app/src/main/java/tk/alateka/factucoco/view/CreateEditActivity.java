e773e84e7c66c0183dc6b06dbae42bc950e1f22a
Alberto Pérez Fructuoso <albertoperez_135@outlook.es>
2022-09-03 00:10:16 +0200

    Modified theme

--- app/src/main/java/tk/alateka/factucoco/view/CreateEditActivity.java
+++ app/src/main/java/tk/alateka/factucoco/view/CreateEditActivity.java
@@ -1,12 +1,15 @@
 package tk.alateka.factucoco.view;
 
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import org.jetbrains.annotations.NotNull;
 import tk.alateka.factucoco.App;
 import tk.alateka.factucoco.R;
 import tk.alateka.factucoco.libs.Utils;
@@ -15,28 +18,37 @@ import tk.alateka.factucoco.model.Invoice;
 public class CreateEditActivity extends AppCompatActivity {
 
     private Invoice invoice;
+    private Utils utils;
+    private int id;
+
+    private TextView inputName;
+    private TextView inputDate;
+    private TextView inputVatRate;
+    private TextView inputVat;
+    private TextView inputAmount;
+    private TextView inputTotal;
+    private TextView inputDescription;
+
+    private MenuItem editItem;
+    private MenuItem createItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_edit);
 
-        Utils utils = new Utils(this);
+        utils = new Utils(this);
 
-        TextView inputName = findViewById(R.id.inputName);
-        TextView inputDate = findViewById(R.id.inputDate);
-        TextView inputVatRate = findViewById(R.id.inputVatRate);
-        TextView inputVat = findViewById(R.id.inputVat);
-        TextView inputAmount = findViewById(R.id.inputAmount);
-        TextView inputTotal = findViewById(R.id.inputTotal);
-        TextView inputDescription = findViewById(R.id.inputDescription);
+        inputName = findViewById(R.id.inputName);
+        inputDate = findViewById(R.id.inputDate);
+        inputVatRate = findViewById(R.id.inputVatRate);
+        inputVat = findViewById(R.id.inputVat);
+        inputAmount = findViewById(R.id.inputAmount);
+        inputTotal = findViewById(R.id.inputTotal);
+        inputDescription = findViewById(R.id.inputDescription);
 
-        Button btnSave = findViewById(R.id.btnSave);
-        Button btnEdit = findViewById(R.id.btnEdit);
-
-        int id = getIntent().getIntExtra("id", 999);
+        id = getIntent().getIntExtra("id", 999);
         if ( id != 999 ) {
-            btnEdit.setVisibility(View.VISIBLE);
             invoice = App.invoices.get(id);
             inputName.setText(invoice.getName());
             inputDate.setText(invoice.getDate());
@@ -45,41 +57,67 @@ public class CreateEditActivity extends AppCompatActivity {
             inputAmount.setText(String.valueOf(invoice.getAmount()));
             inputTotal.setText(String.valueOf(invoice.getTotal()));
             inputDescription.setText(invoice.getDescription());
-        } else
-            btnSave.setVisibility(View.VISIBLE);
+        }
+    }
 
-        btnSave.setOnClickListener(view -> {
-            App.invoices.add(new Invoice(
-                    App.invoices.size(),
-                    inputName.getText().toString(),
-                    inputDate.getText().toString(),
-                    Float.parseFloat(inputVatRate.getText().toString()),
-                    Float.parseFloat(inputVat.getText().toString()),
-                    Float.parseFloat(inputAmount.getText().toString()),
-                    Float.parseFloat(inputTotal.getText().toString()),
-                    inputDescription.getText().toString()
-            ));
-            utils.writeToFactudata();
-            Toast.makeText(this, R.string.modified_invoice, Toast.LENGTH_SHORT).show();
-            finish();
-            Intent back = new Intent(this, MainActivity.class);
-            startActivity(back);
-        });
+    public void createInvoice()
+    {
+        App.invoices.add(new Invoice(
+                App.invoices.size(),
+                inputName.getText().toString(),
+                inputDate.getText().toString(),
+                Float.parseFloat(inputVatRate.getText().toString()),
+                Float.parseFloat(inputVat.getText().toString()),
+                Float.parseFloat(inputAmount.getText().toString()),
+                Float.parseFloat(inputTotal.getText().toString()),
+                inputDescription.getText().toString()
+        ));
+        utils.writeToFactudata();
+        Toast.makeText(this, R.string.created_invoice, Toast.LENGTH_SHORT).show();
+        finish();
+        Intent back = new Intent(this, MainActivity.class);
+        startActivity(back);
+    }
 
-        btnEdit.setOnClickListener(view -> {
-            invoice = App.invoices.get(id);
-            invoice.setName(inputName.getText().toString());
-            invoice.setDate(inputDate.getText().toString());
-            invoice.setVatRate(Float.parseFloat(inputVatRate.getText().toString()));
-            invoice.setVat(Float.parseFloat(inputVat.getText().toString()));
-            invoice.setAmount(Float.parseFloat(inputAmount.getText().toString()));
-            invoice.setTotal(Float.parseFloat(inputTotal.getText().toString()));
-            invoice.setDescription(inputDescription.getText().toString());
-            utils.writeToFactudata();
-            Toast.makeText(this, R.string.modified_invoice, Toast.LENGTH_SHORT).show();
+    public void editInvoice()
+    {
+        invoice = App.invoices.get(id);
+        invoice.setName(inputName.getText().toString());
+        invoice.setDate(inputDate.getText().toString());
+        invoice.setVatRate(Float.parseFloat(inputVatRate.getText().toString()));
+        invoice.setVat(Float.parseFloat(inputVat.getText().toString()));
+        invoice.setAmount(Float.parseFloat(inputAmount.getText().toString()));
+        invoice.setTotal(Float.parseFloat(inputTotal.getText().toString()));
+        invoice.setDescription(inputDescription.getText().toString());
+        utils.writeToFactudata();
+        Toast.makeText(this, R.string.modified_invoice, Toast.LENGTH_SHORT).show();
+        finish();
+        Intent back = new Intent(this, MainActivity.class);
+        startActivity(back);
+    }
+
+    public boolean onCreateOptionsMenu(@NotNull Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_menu, menu);
+        editItem = menu.findItem(R.id.edit_action);
+        createItem = menu.findItem(R.id.create_action);
+        if (id != 999)
+            editItem.setVisible(true);
+        else
+            createItem.setVisible(true);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.create_action) {
+            createInvoice();
+        }
+        if (id == R.id.edit_action) {
+            editInvoice();
+        }
+        if (id == R.id.cancel_action) {
             finish();
-            Intent back = new Intent(this, MainActivity.class);
-            startActivity(back);
-        });
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
